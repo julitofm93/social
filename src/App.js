20a0474f13f9c527ffbc0d9b1d11cabda1d30193
julitofm93 <julian.fm@live.com>
2023-01-13 18:44:12 -0300

    rightbar and darktheme

--- src/App.js
+++ src/App.js
@@ -12,14 +12,15 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile"
-
+import "./style.scss"
+ 
 function App() {
 
   const currentUser = true;
 
   const Layout = () =>{
     return(
-      <div>
+      <div className="theme-dark">
         <NavBar/>
         <div style={{display:"flex"}}>
           <LeftBar/>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,87 @@
-import React from 'react'
+import "./home.scss"
 
 const Home = () => {
   return (
+    <div className="home">
+          <div>Home</div>
     <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    <div>Home</div>
+    </div>
   )
 }
 
